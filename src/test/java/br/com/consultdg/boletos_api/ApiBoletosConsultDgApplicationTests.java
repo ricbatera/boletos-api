fe70df18bad352b9e82f2d33beb2a813517a0196
Ricardo Alves Roberto <ricardo.roberto@rdrtech.com.br>
2024-10-27 13:42:35 -0300

    novo projeto sometne api para os boletos

---
+++ src/test/java/br/com/consultdg/boletos_api/ApiBoletosConsultDgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultdg.boletos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBoletosConsultDgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
